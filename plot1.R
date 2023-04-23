9d09e4bd1ae735024fb955340041691e0e9f46d3
Xuan Lin <linxuan93@icloud.com>
2023-04-22 22:56:08 -0400

    plot1.R

--- plot1.R
+++ plot1.R
@@ -1,7 +1,9 @@
-## read the file into R 
-power <- read.table("./household_power_consumption.txt", header = TRUE, sep = ";")
+## read file into R
 
-##change the class of the variables
+power <- read.table("./household_power_consumption.txt", stringsAsFactors = FALSE, header = TRUE, sep =";"  )
+
+## change the class of the variables 
+
 power$Date <- as.Date(power$Date, format="%d/%m/%Y")
 power$Time <- format(power$Time, format="%H:%M:%S")
 power$Global_active_power <- as.numeric(power$Global_active_power)
@@ -12,11 +14,10 @@ power$Sub_metering_1 <- as.numeric(power$Sub_metering_1)
 power$Sub_metering_2 <- as.numeric(power$Sub_metering_2)
 power$Sub_metering_3 <- as.numeric(power$Sub_metering_3)
 
-newpower <- subset(power, Date == "2007-02-01" | Date == "2007-02-02")
-View(newpower)
+##subset the data set to include only the designated dates
+newpower <- subset(power, Date == "2007-02-01" | Date =="2007-02-02")
 
-## plot histogram of the global active power variable within two days
-png("plot1.png", width = 480, height = 480)
-hist(newpower$Global_active_power, col = "red", border = "black", main = "Global Active Power", xlab = "Global Active Power (kilowatts)", ylab = "Frequency")
+## plot the histogram 
+png("plot1.png", width=480, height=480)
+hist(newpower$Global_active_power, col="red", border="black", main ="Global Active Power", xlab="Global Active Power (kilowatts)", ylab="Frequency")
 dev.off()
-
